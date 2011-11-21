4a7fad8940d6ddd6e671f9849ff73088229a4602
Ji Zhang <jizhang@anjuke.com>
2011-11-21 14:59:46 +0800

    more examples

--- src/pinyin-democli.php
+++ src/pinyin-democli.php
@@ -2,7 +2,7 @@
 require_once dirname(__FILE__) . '/aps/aps-client.php';
 $context = new ZMQContext();
 
-$client = new APSClient($context, array('tcp://127.0.0.1:50000'));
+$client = new APSClient($context, array('tcp://192.168.201.109:50000'));
 
 $client->start_request('convert', array('你好世界'),
     function($reply, $status) {
@@ -10,5 +10,17 @@ $client->start_request('convert', array('你好世界'),
     }, 2000
 );
 
+$client->start_request('convert', array('总部好租技术团队', 'tn'),
+    function($reply, $status) {
+       echo "$status - $reply\n";
+    }, 2000
+);
+
+$client->start_request('convert', array('总部 好租 技术 团队', 'fl', false),
+    function($reply, $status) {
+       echo "$status - $reply\n";
+    }, 2000
+);
+
 echo "Wait for replies\n";
-$pending = APSClient::wait_for_replies(4000);
+$pending = APSClient::wait_for_replies(4);
